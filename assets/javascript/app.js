4f4f6b9ea42c904f5b5a09237a954d4436ddd583
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-12 16:35:51 -0400

    Changes in the global variables.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,15 @@
 // Global Variables
-var timeRemaining;
-var rightAnswer;
+var userInput;
+var correctAnswer;
 var wrongAnswer;
-var intervald;
-var count;
+var question;
+var unAnswer;
+var images;
+var count = 15;
+var slideIndex = 0;
+
+
+// create an array of all the questions and answers
+
+
+// create a function to start the game
