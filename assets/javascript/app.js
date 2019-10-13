e474a189e0c09a491e789963d814daa24dfc0275
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-12 21:19:52 -0400

    Pseudo code certain areas in the code to have a better structure of trivia game.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,8 @@ var slideIndex = 0;
 
 
 // target all your questions from 2-6 and set to them to .hide()
+$("#intro");
+$("#question-1").hide()
 $("#question-2").hide();
 $("#question-3").hide();
 $("#question-4").hide();
@@ -16,6 +18,7 @@ $("#question-5").hide();
 $("#question-6").hide();
 
 // create a function to start the game
+// $("#start-game").click($("#question-1".show()));
 
 
 // Call this function later
@@ -31,4 +34,24 @@ function countDown() {
             $(".time").text('TIMES UP!');
         }
     }, 1500);
+}
+
+function triviaGame() {
+    // if countDown is equal to 0
+    // display the correct answer
+    // add a +1 to unAnswer
+    // display the next question
+    // call the countDown function
+
+    // if userInput is equal to correctAnswer
+    // add +1 to correctAnswer
+    // * display a gif image saying the correct answer, then go to the next question.
+    // call the countDown function
+
+    // if userInput is not equal to correctAnswer
+    // add +1 to wrongAnswer
+    // display a gif saying "Wrong answer" and display the correct answer.
+    // go to the next question
+    // call the countDown function
+
 }
