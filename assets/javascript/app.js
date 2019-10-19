d926b3c32167c6c3fcba3f99b396f3246c4aca88
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-10-19 19:31:38 -0400

    Major changes in the code. Lots of bugs need to be fix.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,106 +1,145 @@
-// Global Variables
-// var userInput;
-// var correctAnswer;
-// var wrongAnswer;
-// var unAnswer;
-// var images;
-// var slideIndex = 0;
-
-// keep this
+// Audio
 var audio = new Audio("game-of-thrones.mp3");
+var stopTimer;
+var intervalId;
+var count = 11;
 
 var trivia = {
-    // declare variables inside of the object
+    // Global variables inside of the object
     correctAnswer: 0,
     wrongAnswer: 0,
     unAnswer: 0,
-    countDown: 20,
-    images: null,
-    current: 0,
-    currentQuestion: 1,
-    currentChoices: 0,
+    images: ['images/Arya.gif', "images/changing-faces.gif", "images/dracarys.gif", "images/ned-stark.gif", "images/night-king.gif", "images/Tyrion.gif"],
+    currentQuestion: 0,
 
     // questions, choices and answers
-    questions: {
-        1: "Who was responsible for the creation of the Night King?",
-        2: "What was Hodor called before he got his tragic door-holding nickname?",
-        3: "Dany's dragons are (or were) called Drogon, Viserion and _____",
-        4: "Where is the House of Black and White, the training temple of the Faceless Men?",
-        5: "Who was Ned Stark's predecessor as Robert Baratheon's Hand?",
-        6: "How many kings and queens of Westeros did Lord Vary's serve?",
-    },
+    questions: [
+        "Who was responsible for the creation of the Night King?",
+        "What was Hodor called before he got his tragic door-holding nickname?",
+        "Dany's dragons are (or were) called Drogon, Viserion and _____",
+        "Where is the House of Black and White, the training temple of the Faceless Men?",
+        "Who was Ned Stark's predecessor as Robert Baratheon's Hand?",
+        "How many kings and queens of Westeros did Lord Vary's serve?",
+    ],
 
-    choices: {
-        1: ["The Lord of Light", "The Drowned God", "The First Men", "The Children of the Forest"],
-        2: ["Myrys", "Wylis", "Horys", "Gladys"],
-        3: ["Dougal", "Vhagar", "Raegal", "Balerion"],
-        4: ["Qarth", "Bravos", "Meeren", "No one knows"],
-        5: ["Jamie Lannister", "Jon Arryn", "Tywin Lannister"],
-        6: [2, 3, , 4, 5]
-    },
+    choices: [
+        ["The Lord of Light", "The Drowned God", "The First Men", "The Children of the Forest"],
+        ["Myrys", "Wylis", "Horys", "Gladys"],
+        ["Dougal", "Vhagar", "Raegal", "Balerion"],
+        ["Qarth", "Bravos", "Meeren", "No one knows"],
+        ["Jamie Lannister", "Jon Arryn", "Tywin Lannister", "Ser Jorah Mormont"],
+        ['Two', "Three", "Four", "Five"]
+    ],
 
-    answers: {
-        1: "The Children of the Forest",
-        2: "Wylis",
-        3: "Raegal",
-        4: "Bravos",
-        5: "Jon Arryn",
-        6: 3,
-    },
+    answers: [
+        "The Children of the Forest",
+        "Wylis",
+        "Raegal",
+        "Bravos",
+        "Jon Arryn",
+        "Three"
+    ]
 }
 
-
-// target all your questions from 2-6 and set to them to .hide()
-$("#intro");
-$("#question-1").hide()
-
-// create a function to start the game
+// function to start the game
 $("#start-game").on('click', function() {
     // inside start game function play the game-of-thrones intro (audio.play())
     // audio.play();
 
-    // Show question the container. 
-    $("#question-1").show();
-    // $('#start-game').hide();
+    // Hide start button
+    $('#start-game').hide();
 
-    questions();
+    triviaGame();
+    showQuestions();
 
-    // begin the coutDown
-    countDown();
 });
 
-function questions() {
-    // Display the ID question
+function triviaGame() {
+
+    $('#options').on('click', 'button', function() {
+        console.log('It works'); // test
+
+        // begin the coutDown
+        // countDown();
+
+        // Check if the answer is correct as user input
+        var user_input = $(this).attr("answer");
+        if (trivia.answers[trivia.currentQuestion] === user_input) {
+
+            // display the status
+            // displayResults();
+
+            // setTimeout(showQuestions, 2000);
+            // setTimeout(displayResults, 4000);
+
+            // $('<div>').html('<img src=' + trivia.images[0] + ">");
+            // $("#image-holder").html("<img src=" + trivia.images[0] + "width='400px'>");
+            $('#status').html('<h3>Correct!!!! ' + trivia.answers[trivia.currentQuestion] + '</h3>');
+
+            // stop the setInterval function
+            stop();
+            stopTimer = clearInterval(intervalId);
+
+            // add +1 to correct answers
+            // trivia.correctAnswer++;
+        } else {
+            // displayResults();
+            // $('#status').html('<h3>Better luck next time! ' + trivia.answers[trivia.currentQuestion] + '</h3>');
+
+            // stop the setInterval function
+            stop();
+            // displayResults();
+            stopTimer = clearInterval(intervalId);
+
+            // call setTimeout function
+            var status = $('#status').html('<h3>The correct answer was ' + trivia.answers[trivia.currentQuestion] + '</h3>');
+            var hideStatus = setTimeout(status, 3000);
+
+            // add +1 to wrong answers
+            // trivia.wrongAnswer++;
+        }
+    });
+}
+
+function showQuestions() {
+    $('#question').show();
+    $('#options').show();
+    $('.time').show();
+
+    // Stop timer
+    intervalId = setInterval(decrement, 1000);
+
     $('#question').html(trivia.questions[trivia.currentQuestion]);
-    trivia.currentQuestion++;
 
-    // $('#options').html(trivia.choices[trivia.currentChoices]);
-    // $('#options').html(trivia.choices[trivia.currentChoices].join('<br>'));
-    // trivia.currentChoices++;
+    var choicesArr = trivia.choices[trivia.currentQuestion];
 
-    var choicesArr = Object.values(trivia.choices)[trivia.currentChoices];
-
-    // Loop through the choices and display it in a button.
+    $('#options').empty();
+    // Loop through the choices and display it as a button.
     for (var i = 0; i < choicesArr.length; i++) {
         var button = $('<button>');
         button.text(choicesArr[i]);
+        button.attr("answer", choicesArr[i]);
         $('#options').append(button);
     }
 
-    // test
     console.log(trivia.questions[trivia.currentQuestion]);
-    console.log(trivia.choices[trivia.currentChoices]);
-    // console.log(choicesArr);
+    console.log(trivia.choices[trivia.currentQuestion]);
+    console.log(trivia.answers[trivia.currentQuestion]);
 }
 
+function displayResults() {
+    // hide the question, options, and countDown
+    $('#question').hide();
+    $('#options').hide();
+    $('.time').hide();
 
-var count = 21;
-var intervalId;
+    // Display a countDown when the next question is going to appear. 5 seconds max.
+    // stop();
+    stopTimer = clearInterval(intervalId);
+    // trivia.currentQuestion++;
 
-function countDown() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-
+    setTimeout(showQuestions, 3000);
+    count = 11;
 }
 
 function decrement() {
@@ -108,54 +147,36 @@ function decrement() {
     count--;
 
     // Diplsay the count down in the #countDown
-    $("#countDown").text(count + ' seconds');
+    $("#countDown").text("Time remaining " + count + ' seconds');
 
-    // when the number hits zero
+    // if countDown is equal to 0
     if (count === 0) {
+        // tell the user time is up!
+        // $(".time").text('TIMES UP!');
+
         // call the stop function
         stop();
+        stopTimer = clearInterval(intervalId);
 
-        // tell the user time is up!
-        $(".time").text('TIMES UP!');
+        // display the correct answer
+        displayResults();
+        $('#status').html('<h3>The correct answer was ' + trivia.answers[trivia.currentQuestion] + '</h3>');
+
+        // display the next question
+        trivia.currentQuestion++;
+
+        // add a +1 to unAnswer
+        trivia.unAnswer++;
     }
     console.log(count);
 }
 
+
 function stop() {
-    clearInterval(intervalId);
+    // clearInterval(intervalId);
+    // triviaGame();
+    // decrement();
+    // showQuestions();
+    displayResults();
+    trivia.currentQuestion++;
 }
-
-
-function nextQuestion() {
-
-    // $("#question").html(questions[0]);
-
-    // if countDown is equal to 0
-    // display the correct answer
-    // add a +1 to unAnswer
-    // display the next question
-    // call the countDown function
-
-    // if userInput is equal to correctAnswer
-    // add +1 to correctAnswer
-    // * display a gif image saying the correct answer, then go to the next question.
-    // call the countDown function
-
-    // if userInput is not equal to correctAnswer
-    // add +1 to wrongAnswer
-    // display a gif saying "Wrong answer" and display the correct answer.
-    // go to the next question
-    // call the countDown function
-
-}
-
-// * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
-
-// * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
-
-// * The scenario is similar for wrong answers and time-outs.
-
-//   * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-//   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-
-// * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
